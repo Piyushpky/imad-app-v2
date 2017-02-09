8641d4b00de2b34c1a8e431c1ec969cce5b009cb
Piyush Kumar <99pky99@gmail.com>
2017-02-10 01:58:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article1',function(req,res){
+   res.send('article one requested and will be served'); 
+});
 
+app.get('/article2',function(req,res){
+   res.send('article two requested and will be served'); 
+});
+
+app.get('/article3',function(req,res){
+   res.send('article three requested and will be served'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
