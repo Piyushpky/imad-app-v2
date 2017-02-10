d04aeb02de74e65ea1d5977d6da0b4b6dfa91382
Piyush Kumar <99pky99@gmail.com>
2017-02-11 02:54:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('maint');
 element.innerHTML='new Value';
 var img=document.getElementById('mad');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+    
 };
