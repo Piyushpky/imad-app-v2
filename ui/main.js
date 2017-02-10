a3dc2316a515669c8927f33f218df9929850eb56
Piyush Kumar <99pky99@gmail.com>
2017-02-11 02:48:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element=document.getElementById('maint');
 element.innerHTML='new Value';
 var img=document.getElementById('mad');
-img.onClick=fumction(){
+img.onclick=function(){
     img.style.marginLeft='100px';
 };
