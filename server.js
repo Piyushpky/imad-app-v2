2b8c647b7afab62505e2ece6fc9bb2766fc0f3df
Piyush Kumar <99pky99@gmail.com>
2017-02-11 12:57:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,10 @@ var htmlTemp=`<html>
 `;
 return htmlTemp;
 }
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+})
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -90,10 +94,7 @@ app.get('/:articleName',function(req,res){
    res.send(createTemplate(articles[articleName]));
 });
 var counter=0;
-app.get('/counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-})
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
