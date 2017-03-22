64a9b4858e521d9522d855e0d5d1d122233e100b
Piyush Kumar <99pky99@gmail.com>
2017-03-22 23:26:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,9 +117,21 @@ app.get('/test-db',function(req,res){
    });
    
 });
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+app.get('/article/:articleName',function(req,res){
+    pool.query("SELECT * FROM article WHERE title="+req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+            
+        }else{
+            if(result.rows.length==0){
+                result.status(404).send('article Not Found');
+            }else{
+                var  articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+   
 });
 var counter=0;
 
